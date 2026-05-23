a4d7bd5fee2d542fdcca7385b9641fa6510af7df
GaneshSalunke1 <93383643+GaneshSalunke1@users.noreply.github.com>
2026-05-23 10:54:33 +0530

    if else elif

---
+++ ifelseelif.py
@@ -0,0 +1,34 @@
+# userdata = 11
+
+# if(userdata == 10):
+#     print(f"got {userdata}")
+# elif(userdata > 10):
+#     print(f"got more than {userdata}")
+# else:
+#     print(f"got less than {userdata}")
+
+##user input
+
+# userinput = int(input('Enter student marks'))
+
+#userinput = 0
+
+
+# if(userinput == 0):
+#     print('its not even and not odd its zero number')
+# elif(userinput%2):
+#     print('Its Odd Number')
+# else:
+#     print('its even number')
+
+# print(f'userinput is {userinput}')
+
+input1 = int(input("Enter number 1 \n"))
+input2 = int(input("Enter number 2 \n"))
+
+if(input1 == input2):
+    print('Both Numbers are same')
+elif(input1 > input2):
+    print('Input1 is greater than Input2')
+else:
+    print('Input2 is greater than Input1')
